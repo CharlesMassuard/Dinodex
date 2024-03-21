06db31695f62260607a7352c031a632499baab25
Ludmann Dorian <dorianludm7@gmail.com>
2024-03-21 09:17:27 +0100

    Render des dinosaures améilorrer jusqu'au images

--- views/allDinosaurs.js
+++ views/allDinosaurs.js
@@ -5,16 +5,22 @@ export default class AllDinosaurs {
     async render() {
         let dinosaursList = document.querySelector("#dinosaurs");
         let dinosaurs = await DinosaursProvider.fetchDinosaurs();
+        let ol = document.createElement("ol");
+
         dinosaurs.forEach(dinosaur => {
             let li = document.createElement("li");
             li.innerHTML = `
                 <a href="#dinosaurs/${dinosaur.id}">
+                    <img src="${dinosaur.image}" alt="${dinosaur.nom}">
                     <h2>${dinosaur.nom}</h2>
+                    <p>Type: ${dinosaur.type}</p>
+                    <p>Tameable: ${dinosaur.tameable ? 'Yes' : 'No'}</p>
+                    <p>Food: ${dinosaur.food.join(', ')}</p>
                 </a>
             `;
-            dinosaursList.appendChild(li);
+            ol.appendChild(li);
         });
-        document.body.appendChild(view);
-        return view;
+        dinosaursList.appendChild(ol);
+        return dinosaursList;
     }
 }
